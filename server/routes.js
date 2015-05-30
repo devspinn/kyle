7d6fa77e0b9d8a1fc3fb3077f605678ff8a15e9e
dtown <devonstownsend@gmail.com>
2015-05-29 21:50:19 -0700

    photos api

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/api/photos', require('./api/photo'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
