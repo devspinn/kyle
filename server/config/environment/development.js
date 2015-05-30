ab79f0d1f0bcd941049c190b55defe6c997f0b91
Ubuntu <ubuntu@ip-172-31-47-132.us-west-2.compute.internal>
2015-05-30 04:44:15 +0000

    first

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri:'mongodb://dtown:reedmpkdtown@ds041032.mongolab.com:41032/kyle'
+  },
+
+  seedDB: true
+};
